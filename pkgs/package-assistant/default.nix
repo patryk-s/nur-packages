1dd8426d89e3ea971bd10c2901f813cb120f4ba6
patryk-s <patryk.sidzina@gmail.com>
2025-05-22 17:49:34 +0200

    Added package-assistant 1.0.0

---
+++ pkgs/package-assistant/default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  fetchFromGitHub,
+  rustPlatform,
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "package-assistant";
+  version = "1.0.0";
+
+  src = fetchFromGitHub {
+    owner = "patryk-s";
+    repo = pname;
+    rev = "refs/tags/${version}";
+    hash = "sha256-v4cC/QQ6CEn5kCju39taSVMj6MAUhMGGj+OYveEe5XE=";
+  };
+
+  cargoHash = "sha256-8OOK/8XQOsnq+HBOwqewEv4Otf/oyXyUhyYyiESCcLw=";
+
+  meta = {
+    description = "Manage your package managers";
+    longDescription = ''
+      Provides a consistent CLI interface for all supported package managers,
+      across multiple OSes, so you don't have to remember the specific syntax on a given system.
+    '';
+    homepage = "https://github.com/patryk-s/package-assistant";
+    license = lib.licenses.mit;
+    # maintainers = [ ];
+    mainProgram = "pa";
+  };
+}
